e2ece8ea9d1bcd617d45f9327aed293a05e11f86
GrCOTE7 <GrCOTE7@GMail.com>
2016-06-17 16:11:25 +0200

    Ini tuto

--- gc7.php
+++ gc7.php
@@ -3,16 +3,17 @@
 <head>
   <title>Tuto</title>
   <script src=" js/jquery-3.0.0.min.js"></script>
-  <script src="uikit/docs/js/uikit.min.js"></script>
   <link rel="stylesheet" href="uikit/src/less/uikit.css"/>
-  <script src="uikit/src/js/components/htmleditor.js"></script>
   <link rel="stylesheet" href="css/gc7.css">
 </head>
 
 <body>
+
 <div id="maingc7">
   <h1>Tuto</h1>
+  <p>Ok</p>
 
 </div>
+<script src="uikit/docs/js/uikit.min.js"></script>
 </body>
 </html>
