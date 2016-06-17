99ba39b0e995f59c657a19dd405d4bd4327a2510
GrCOTE7 <GrCOTE7@GMail.com>
2016-06-17 18:03:31 +0200

    ca - tuto jQ

--- gc7.js
+++ gc7.js
@@ -1,44 +1,21 @@
-(function ($) {
-    $(document).on('ready', function () {
-        var sortable = $('[data-uk-sortable]'),
-            button = $('button');
-        button.click(function () {
-            saveOrdering(sortable, button);
-        });
-        sortable.on('stop.uk.sortable', function (e, el, type) {
-            setOrdering(sortable, el);
-        });
-        setOrdering(sortable);
-    });
-    function setOrdering(sortable, activeEl) {
-        var ordering = 1;
-        sortable.find('>li').each(function () {
-            var $ele = $(this);
-            $ele.data('ordering', ordering);
-            $ele.find('div.uk-badge').text(ordering);
-            ordering++;
-        });
-        if (activeEl) {
-            activeEl.find('div.uk-badge').addClass('uk-animation-scale-down');
-        }
-    }
-    function saveOrdering (sortable, button) {
-        var url = 'index.php',
-            data = {
-                task: 'saveOrdering',
-                ordering: {}
-            };
-        sortable.find('>li').each(function () {
-            data.ordering[$(this).data('id')] = $(this).data('ordering');
-        });
-        button.prop('disabled', true);
-        console.log(data); //data going to server
-        $.getJSON(url, data, function (result) {
-            console.log(result); //json response from server
-            button.prop('disabled', false);
-        });
-        setTimeout(function(){button.prop('disabled', false);},1000);//for testing only!
-    }
-})(jQuery);
+$(function () {
+    $(document).ready(function () {
 
-UIkit.ready(function() {UIkit.$body.prepend('<div class="uk-float-right uk-badge">UIkit version ' + UIkit.version + '</div>')});
+
+        $('#button').click(function () {
+            var v = $('input[name=checkListItem]').val();
+            console.log(v);
+            $('h2').css('color', 'blue');
+
+            $('.list').append('<div class="item">' +
+                '<input type="checkbox">' + v
+                + '</div>');
+
+        }).click().click().click();
+
+        $(document).on('click', '.item', function () {
+            $(this).remove();
+        })
+
+    });
+});
--- gc7.php
+++ gc7.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Tuto</title>
+  <title>Tuto jQ</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="uikit/src/less/uikit.css"/>
@@ -10,14 +10,22 @@
 
 <body>
 
-<div id="maingc7">
-  <h1>Tuto</h1>
+<section id="maingc7">
+
+  <h2>To Do</h2>
+  <form name="checkListForm">
+    <input type="text" name="checkListItem" value="Oki">
+  </form>
+  <button id="button">Add!</button>
+  <br/>
+  <br/>
+  <div class="list"></div>
 
 
-</div>
+</section>
 <script src=" js/jquery-3.0.0.min.js"></script>
 <script src="uikit/docs/js/uikit.min.js"></script>
-<script src="uikit/src/js/components/sortable.js"></script>
+<!--<script src="uikit/src/js/components/sortable.js"></script>-->
 <script src="gc7.js"></script>
 </body>
 </html>
