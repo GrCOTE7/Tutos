c5ba94bcee3fac960e45309e2f5b1025600af713
GrCOTE7 <GrCOTE7@GMail.com>
2016-07-23 13:14:47 +0200

    Objet Math

--- js/div.js
+++ js/div.js
@@ -8,4 +8,20 @@ grCote7.tools.chrono.start();
     aff('l ds lionel: ' + myRegex.test(str));
 
 })();
+
+var arr = [1, 2, 3],
+    o2T = function () { // foret + TAB ds IDE
+        var n = 0, t = '<table>';
+        for (var id in arr) {
+            n++;
+            if (arr.hasOwnProperty(id)) {
+                t += '<tr><td>' + id + '</td>' +
+                    '<td>' + arr[id] + '</td></tr>';
+            }
+        }
+        t += '</table>';
+        aff(n + ' items: ' + t);
+    };
+o2T();
+
 grCote7.tools.chrono.stop();
